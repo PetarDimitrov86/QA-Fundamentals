5bbaf2f93c421a9270fd8413dc2583f393643aa9
Petar Dimitrov <lbmikesmith@gmail.com>
2017-03-21 10:18:15 +0200

    Add files via upload

---
+++ SeleniumTest/ConsoleTest/AbvLoginTest.cs
@@ -0,0 +1,34 @@
+﻿namespace SeleniumTest
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System;
+    using System.Threading;
+
+    class AbvLoginTest
+    {
+        static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.abv.bg/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("username")).SendKeys("qasoftunirandom");
+            driver.FindElement(By.Id("password")).SendKeys("QAsoftuni");
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("loginBut")).Click();
+            Thread.Sleep(3000);
+            string expectedUsername = "Kichka Bodurova";
+            string actualUsername = driver.FindElement(By.ClassName("userName")).Text;
+
+            if (expectedUsername == actualUsername)
+            {
+                Console.WriteLine("Actual username matches expected username");
+            }
+            else
+            {
+                Console.WriteLine("Usernames do not match");
+            }
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumTest/UnitTest/AbvSignInTest.cs
@@ -0,0 +1,29 @@
+﻿namespace UnitTestProject2
+{
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    [TestClass]
+    public class AbvSignInTest
+    {
+        [TestMethod]
+        public void TryLogin()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.abv.bg/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("username")).SendKeys("qasoftunirandom");
+            driver.FindElement(By.Id("password")).SendKeys("QAsoftuni");
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("loginBut")).Click();
+            Thread.Sleep(3000);
+            string expectedUsername = "Kichka Bodurova";
+            string actualUsername = driver.FindElement(By.ClassName("userName")).Text;
+
+            Assert.AreEqual(expectedUsername, actualUsername);
+            driver.Quit();
+        }
+    }
+}
