3334003572f52a09cf5b0fe3db843f8bf01c6024
Petar Dimitrov <lbmikesmith@gmail.com>
2017-03-25 13:00:53 +0200

    Add files via upload

---
+++ SeleniumTest/UnitTest/WithExplicitWaiting.cs
@@ -0,0 +1,32 @@
+﻿namespace SeleniumTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System;
+    using OpenQA.Selenium.Support.UI;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TryLogin()
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("http://www.abv.bg/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("username")).SendKeys("qasoftunirandom");
+            driver.FindElement(By.Id("password")).SendKeys("QAsoftuni");
+            wait.Until(ExpectedConditions.ElementExists(By.Id("loginBut")));
+            driver.FindElement(By.Id("loginBut")).Click();
+
+            wait.Until(ExpectedConditions.ElementExists(By.ClassName("userName")));
+            string expectedUsername = "Kichka Bodurova";
+            string actualUsername = driver.FindElement(By.ClassName("userName")).Text;
+
+            Assert.AreEqual(expectedUsername, actualUsername);
+            driver.Quit();
+        }
+    }
+}
